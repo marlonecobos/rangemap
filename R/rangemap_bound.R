f7f41a840b0447c37f5e37991c28e6ba027b094f
Marlon E. Cobos <manubio13@gmail.com>
2018-05-22 10:46:16 -0500

    V 0.0.6 Update dissolve paramter working

--- R/rangemap_bound.R
+++ R/rangemap_bound.R
@@ -2,19 +2,23 @@
 #'
 #' @description rangemap_bound generates a species range polygon for a given species
 #' by considering all the polygons of political entities in which the species has
-#' been detected. Shape files can be saved in the working directory if it is needed.
+#' been detected. The species extent of occurrence and area of occupancy according to
+#' the IUCN are also generated. Shape files can be saved in the working directory if it is needed.
 #'
 #' @param occurrences a data.frame containing species occurrences, columns must be:
 #' Species, Longitude, and Latitude.
 #' @param country_code (character) vector of country codes that will be considered when
-#' creating the species range. Codes follow the ISO-3166-1 norm as in function \code{\link[raster]{getData}}.
+#' creating the species range. Note that including neighbor countries may be necessary to calculate
+#' the extent of occurrence based on the IUCN criteria, even if they are not occupied by the species.
+#' Codes follow the ISO-3166-1 norm as in function \code{\link[raster]{getData}}.
 #' @param boundary_level (numeric) the level of the administrative boundaries from 0 to 5 (0 is
 #' the country and higher values equal finer divisions), default = 0.
 #' @param polygons (optional) a SpatialPolygon object that will be used instead of boundaries
 #' to create species ranges based on overlapping of species records with these layer. If defined,
 #' argument boundaries will not be considered.
-#' @param disolve (logical) if TRUE distint polygons selected as part of the species range will
-#' be disolved for creating simpler polygons, default = TRUE.
+#' @param dissolve (logical) if TRUE distint polygons selected as part of the species range will
+#' be disolved for creating simpler polygons, default = TRUE. The dissolving process may take some
+#' more time, especially if species with broad distributions are being analyzed.
 #' @param save_shp (logical) if TRUE shapefiles of the species range, extent of occurrence and area of
 #' occupancy will be written in the working directory.
 #' @param name (character) valid if save_shp TRUE. The name of the shapefile to be exported.
@@ -39,13 +43,13 @@
 #'             c("name", "decimalLongitude", "decimalLatitude")]
 #'
 #' level <- 0
-#' disolve <- TRUE
+#' dissolve <- TRUE
 #' save <- TRUE
 #' name <- "test"
-#' countries <- c("PER", "BRA", "COL", "VEN", "ECU", "GUF", "GUY", "SUR")
+#' countries <- c("PER", "BRA", "COL", "VEN", "ECU", "GUF", "GUY", "SUR", "BOL")
 #'
 #' bound_range <- rangemap_bound(occurrences = occ_g, country_code = countries, boundary_level = level,
-#'                               disolve = disolve, save_shp = save, name = name)
+#'                               dissolve = dissolve, save_shp = save, name = name)
 
 # Dependencies: maps (map),
 #               maptools (map2SpatialPolygons),
@@ -56,7 +60,7 @@
 #                   CRS, over, Polygons, Polygon, SpatialPolygons, proj4string)
 
 rangemap_bound <- function(occurrences, country_code, boundary_level = 0,
-                           polygons, disolve = TRUE, save_shp = FALSE, name) {
+                           polygons, dissolve = TRUE, save_shp = FALSE, name) {
   # testing potential issues
   if (missing(occurrences)) {
     stop("Argument occurrences is necessary to perform the analysis")
@@ -83,6 +87,8 @@ rangemap_bound <- function(occurrences, country_code, boundary_level = 0,
     polygon <- do.call("rbind", bounds)
     polygon@data$OBJECTID <- 1:length(bounds)
     polygons <- sp::spTransform(polygon, WGS84)
+
+    polygons@data[, 6:dim(polygons@data)[2]] <- NULL # erase non-relevant columns to avoid to much data
   }
 
   # erasing rds files in working directory
@@ -106,12 +112,21 @@ rangemap_bound <- function(occurrences, country_code, boundary_level = 0,
   # select polygons that overlap with points
   boundaries <- polygons[!is.na(sp::over(polygons, as(occ_pr, "SpatialPoints"))), ]
 
-  # disolve?
+  # disolve
+  if (dissolve == TRUE) {
+    cat("\nDissolving polygons, please wait...\n")
+    polygons@data$union_field <- rep("Union", length(polygons@data[, 1])) # new field for union
 
+    polygons <- rgeos::gUnaryUnion(polygons, id = polygons@data$union_field) # now dissolve
+
+    boundaries@data$union_field <- rep("Union", length(boundaries@data[, 1])) # new field for union
+
+    boundaries <- rgeos::gUnaryUnion(boundaries, id = boundaries@data$union_field) # now dissolve
+  }
 
   # calculate areas in km2
-  areas_km2 <- raster::area(boundaries) / 1000000
-  area_km2 <- sum(areas_km2) # total area of the species range
+  rangekm2 <- raster::area(boundaries) / 1000000
+  area_km2 <- sum(rangekm2) # total area of the species range
 
   ## extent of occurrence
   coord <- as.data.frame(occ[, 2:3]) # spatial point dataframe to data frame keeping only coordinates
@@ -122,43 +137,43 @@ rangemap_bound <- function(occurrences, country_code, boundary_level = 0,
   covexhull_polygon_pr <- sp::spTransform(covexhull_polygon, AEQD) # reproject
   c_hull_extent <- rgeos::gIntersection(polygons, covexhull_polygon_pr, byid = TRUE, drop_lower_td = TRUE) # area of interest
 
-  ext_occ_area_km2 <- raster::area(c_hull_extent) / 1000000
-  extent_occ_km2 <- sum(ext_occ_area_km2) # total area of the species range
+  eockm2 <- raster::area(c_hull_extent) / 1000000
+  eocckm2 <- sum(eockm2) # total area of the species range
 
   ## area of occupancy
   grid <- raster::raster(ext = raster::extent(occ_pr) + 10000, res = c(2000, 2000), crs = AEQD)
   raster_sp <- raster::rasterize(occ_pr[, 2:3], grid)[[1]] # raster from points
   grid_sp <- as(raster_sp, "SpatialPolygonsDataFrame") # raster to polygon
 
-  area_occ_area_km2 <- raster::area(grid_sp) / 1000000
-  area_occ_km2 <- sum(area_occ_area_km2) # area calculation
+  aockm2 <- raster::area(grid_sp) / 1000000
+  aocckm2 <- sum(aockm2) # area calculation
 
   # adding characteristics to spatial polygons
   species <- as.character(occurrences[1, 1])
-  boundaries <- sp::SpatialPolygonsDataFrame(boundaries, data = data.frame(species, areas_km2, # species range
-                                                                           extent_occ_km2,
-                                                                           area_occ_km2),
+  boundaries <- sp::SpatialPolygonsDataFrame(boundaries, data = data.frame(species, rangekm2, # species range
+                                                                           eocckm2,
+                                                                           aocckm2),
                                             match.ID = FALSE)
 
   extent_occurrence <- sp::SpatialPolygonsDataFrame(c_hull_extent, # extent of occurrence
                                                     data = data.frame(species,
-                                                                      ext_occ_area_km2),
+                                                                      eockm2),
                                                     match.ID = FALSE)
 
   area_occupancy <- sp::SpatialPolygonsDataFrame(grid_sp, data = data.frame(species, # area of occupancy
-                                                                            area_occ_area_km2),
+                                                                            aockm2),
                                                  match.ID = FALSE)
 
   # exporting
   if (save_shp == TRUE) {
-    cat("\nWriting shapefiles in the working directory.\n")
+    cat("\nWriting shapefiles in the working directory...\n")
     rgdal::writeOGR(boundaries, ".", name, driver = "ESRI Shapefile")
     rgdal::writeOGR(extent_occurrence, ".", paste(name, "extent_occ", sep = "_"), driver = "ESRI Shapefile")
     rgdal::writeOGR(area_occupancy, ".", paste(name, "area_occ", sep = "_"), driver = "ESRI Shapefile")
   }
 
   # return results (list or a different object?)
-  sp_dat <- data.frame(occ[1, 1], dim(occ_pr)[1], area_km2, extent_occ_km2, area_occ_km2) # extent of occ = total area?
+  sp_dat <- data.frame(occ[1, 1], dim(occ_pr)[1], rangekm2, eocckm2, aocckm2) # extent of occ = total area?
   names(sp_dat) <- c("Species", "Non-duplicate records", "Range area", "Extent of occurrence", "Area of occupancy")
 
   results <- list(sp_dat, boundaries, extent_occurrence, area_occupancy)
