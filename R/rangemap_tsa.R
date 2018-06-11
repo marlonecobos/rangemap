462f64831c3dd9925fefd8499832a171384e46b6
Marlon E. Cobos <manubio13@gmail.com>
2018-06-11 17:56:41 -0500

    V 0.0.17 rangemap_tsa begenning

--- R/rangemap_tsa.R
+++ R/rangemap_tsa.R
@@ -5,19 +5,24 @@
 #'
 #' @param occurrences a data.frame containing species occurrences, columns must be:
 #' Species, Longitude, and Latitude.
-#' @param variables a RasterStack object of environmental variables that will be used for
-#' performing the trend surface analysis.
+#' @param region_of_interest a SpatialPolygon object on which the trend surface analysis
+#' will be performed.
+#' @param resolution (numeric) resolution in km in which the resultant surface will be created,
+#' default = 5.
 #' @param threshold (numeric) percentage of occurrence records to be excluded when deciding
-#' the minimum surface value to be considered part of the species range, default = 5.
-#' @param export (logical) if TRUE a shapefile of the species range will be written in the working
-#' directory, appart of the returned object.
+#' the minimum surface value to be considered part of the species range, default = 0.
+#' @param save_shp (logical) if TRUE shapefiles of the species range, extent of occurrence and area of
+#' occupancy will be written in the working directory.
+#' @param name (character) valid if save_shp TRUE. The name of the shapefile to be exported.
 #'
 #' @return A named list containing a data.frame with information about the species range, a
 #' SpatialPolygon object of the species range in Geographic projection, and the same SpatialPolygon
 #' object projected to the Azimuthal equal area projection.
 #'
 #' @details Trend surface analysis Is a method based on low-order polynomials of spatial coordinates
-#' for estimating a regular grid of points from scattered observations.
+#' for estimating a regular grid of points from scattered observations. This method assumes that all
+#' cells not occupied by occurrences are absences; hence its use depends on the quality of data and
+#' the certainty of having or not a complete sampling of the regiong_of_interest.
 #'
 #' @examples
 
@@ -25,7 +30,7 @@
 #               raster (buffer, area), maps (map), maptools (map2SpatialPolygons),
 #               rgeos (gUnaryUnion, gIntersection, gCentroid), saptial?
 
-rangemap_tsa <- function(occurrences, variables, threshold = 5, export = FALSE) {
+rangemap_tsa <- function(occurrences, region_of_interest, threshold = 0, save_shp = FALSE, name) {
   # perform a tsa using species records and environmental variables
   # give a value to get the area
   # calculate areas (number of pixels times resolution)
