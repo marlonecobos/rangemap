4f9861c1e494eeb5a4a2249f53af5d129635802e
Marlon E. Cobos <manubio13@gmail.com>
2018-06-21 11:33:14 -0500

    V 0.0.23 update rangemap_bound improved

--- R/rangemap_bound.R
+++ R/rangemap_bound.R
@@ -98,11 +98,11 @@ rangemap_bound <- function(occurrences, country_code, boundary_level = 0,
     polygons <- sp::spTransform(polygon, WGS84)
 
     polygons@data[, 6:dim(polygons@data)[2]] <- NULL # erase non-relevant columns to avoid to much data
-  }
 
-  # erasing rds files in working directory
-  erase_rds <- list.files(path = ".", pattern = "^GADM_", full.names = TRUE)
-  unlink(erase_rds)
+    # erasing rds files in working directory
+    erase_rds <- list.files(path = ".", pattern = "^GADM_", full.names = TRUE)
+    unlink(erase_rds)
+  }
 
   # keeping only records in land
   occ_sp <- occ_sp[!is.na(sp::over(occ_sp, as(polygons, "SpatialPolygons"))), ]
