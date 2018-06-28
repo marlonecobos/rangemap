729a61583759093439e5d355533fa6b1035ceeba
Marlon E. Cobos <manubio13@gmail.com>
2018-06-28 16:08:19 -0500

    V 0.0.26 country_names data added

---
+++ R/country_names.R
@@ -0,0 +1,14 @@
+#' List of country names and ISO codes
+#'
+#' A dataset containing codes for identifying countries according to ISO
+#' norms.
+#'
+#' @format A data frame with 247 rows and 4 columns.
+#' \describe{
+#'   \item{Country_or_Area_Name}{character, names.}
+#'   \item{ISO_ALPHA.2_Code}{character, country code.}
+#'   \item{ISO_ALPHA.3_Code}{character, country code.}
+#'   \item{ISO_Numeric_Code_UN_M49_Numerical_Code}{numeric, country numeric codes.}
+#' }
+#' @source \url{http://www.nationsonline.org/oneworld/country_code_list.htm}
+"country_names"
