754592bb14804117fc793fc3ac3f8c397757dad5
marlonecobos <manubio13@gmail.com>
2020-05-16 15:17:44 -0500

    V 0.1.4 occ P empusa added to data and doc

---
+++ R/occ_doc.R
@@ -0,0 +1,16 @@
+#' Occurrence records for the species Peltophryne empusa
+#'
+#' @description A dataset containing geographic corrdinates of a Caribean toad.
+#'
+#' @format A data frame with 182 rows and 3 columns.
+#' \describe{
+#'   \item{name}{character, species scientific name.}
+#'   \item{longitude}{numeric, longitude values.}
+#'   \item{latitude}{numeric, latitude values.}
+#' }
+#' @source \url{https://www.gbif.org/}
+#'
+#' @examples
+#' data(occ_p)
+#' head(occ_p)
+"occ_p"
