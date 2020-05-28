a851ee444cfe5c9257638ad7472a57e91f1589c6
marlonecobos <manubio13@gmail.com>
2020-05-28 17:07:31 -0500

    V 0.1.6 general update of rangemap_hull process and documentation

--- R/helpers.R
+++ R/helpers.R
@@ -39,40 +39,74 @@ simple_wmap <- function(which = "simplest") {
 }
 
 
-#' Prepare Lambert Azimuthal Equal-Area Projection
-#' @param occ matrix or SpatialPoint object conaining coordinates to serve as
-#' a reference for the center of the projection. If not defined projection is
-#' centered in 0, 0 for longitude and latitude. If a matrix is used columns must
+#' Prepare Equal-Area or Equidistante Projection
+#' @param occurrences matrix or data.frame conaining coordinates to serve as
+#' a reference for the center of the projection. Columns must be:
 #' "longitude" and "latitude", in that order.
+#' @param spatial_onject Spatial* objects, Poinst or Polygons, to be used to
+#' calculate a reference for the center of the projection. Projection must be
+#' WGS84 (EPSG:4326).
+#' @details If arguments are not defined projection is centered in 0, 0 for
+#' longitude and latitude.
 #' @return An object of class CRS.
 #' @export
+#' @importFrom rgeos gCentroid
 #' @importFrom sp CRS
 #' @examples
 #' LAEA_projection()
 #'
 #' data("occ_p", package = "rangemap")
 #' occ <- unique(occ_p)[, 2:3]
-#' LAEA_projection(occ)
+#' AED_projection(occ)
+#' @rdname LAEA_projection
 
-LAEA_projection <- function(occ = NULL) {
-  if (!is.null(occ)) {
-    if (!class(occ)[1] %in% c("matrix", "SpatialPointsDataFrame", "SpatialPoints")) {
-      stop("Class 'occ' must be either one of the following:\n'matrix', 'SpatialPointsDataFrame', or 'SpatialPoints'")
+LAEA_projection <- function(occurrences = NULL, spatial_onject = NULL) {
+  if (!is.null(occurrences) | !is.null(spatial_onject)) {
+    if (!is.null(occurrences)) {
+      if (!class(occurrences)[1] %in% c("matrix", "data.frame", "tbl_df")) {
+        stop("Argument 'occurrences' is not valid, see functions help.")
+      }
+      cent <- apply(occ, 2, mean)
     } else {
-      if (class(occ)[1] == "matrix") {
-        cent <- apply(occ, 2, mean)
+      if (!class(spatial_onject)[1] %in% c("SpatialPointsDataFrame", "SpatialPoints",
+                                           "SpatialPolygonsDataFrame", "SpatialPolygons")) {
+        stop("Argument 'spatial_onject' is not valid, see functions help.")
       } else {
-        cent <- apply(occ@coords, 2, mean)
+        cent <- rgeos::gCentroid(spatial_onject, byid = FALSE)@coords
       }
     }
     LAEA <- sp::CRS(paste0("+proj=laea +lat_0=", cent[2], " +lon_0=", cent[1],
-                           " +x_0=0 +y_0=0 +ellps=GRS80",
-                           " +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
+                           " +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
   } else {
-    LAEA <- sp::CRS(paste("+proj=laea +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=GRS80",
-                          "+towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
+    LAEA <- sp::CRS(paste("+proj=laea +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84",
+                          "+datum=WGS84 +units=m +no_defs"))
   }
   return(LAEA)
+}
+
+#' @rdname LAEA_projection
+AED_projection <- function(occurrences = NULL, spatial_onject = NULL) {
+  if (!is.null(occurrences) | !is.null(spatial_onject)) {
+    if (!is.null(occurrences)) {
+      if (!class(occurrences)[1] %in% c("matrix", "data.frame", "tbl_df")) {
+        stop("Argument 'occurrences' is not valid, see functions help.")
+      }
+      cent <- apply(occ, 2, mean)
+    } else {
+      if (!class(spatial_onject)[1] %in% c("SpatialPointsDataFrame", "SpatialPoints",
+                                           "SpatialPolygonsDataFrame", "SpatialPolygons")) {
+        stop("Argument 'spatial_onject' is not valid, see functions help.")
+      } else {
+        cent <- rgeos::gCentroid(spatial_onject, byid = FALSE)@coords
+      }
+    }
+    AEQD <- sp::CRS(paste0("+proj=aeqd +lat_0=", cent[2], " +lon_0=", cent[1],
+                          " +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
+  } else {
+    AEQD <- sp::CRS(paste("+proj=aeqd +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84",
+                          "+datum=WGS84 +units=m +no_defs"))
+  }
+  return(AEQD)
 }
 
 
@@ -143,10 +177,10 @@ GADM_spoly <- function(country_code, boundary_level, keep_data = FALSE) {
 eoo <- function(occurrences, polygons) {
   WGS84 <- sp::CRS("+init=epsg:4326")
   species <- as.character(occurrences[1, 1])
-  coord <- as.data.frame(occurrences[, 2:3]) # spatial point dataframe to data frame keeping only coordinates
+  coord <- as.data.frame(occurrences[, 2:3])
   covexhull <- chull(coord) # convex hull from points
   coord_pol <- coord[c(covexhull, covexhull[1]), ] # defining coordinates
-  covexhull_polygon <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(coord_pol)), ID = 1))) # into SpatialPolygons
+  covexhull_polygon <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(coord_pol)), ID = 1)))
   sp::proj4string(covexhull_polygon) <- WGS84 # project
   covexhull_polygon_pr <- sp::spTransform(covexhull_polygon, polygons@proj4string) # reproject
   c_hull_extent <- rgeos::gIntersection(polygons, covexhull_polygon_pr,
@@ -250,7 +284,7 @@ aoo <- function(occ_pr, species) {
 #' occ_sp <- sp::SpatialPointsDataFrame(coords = occ[, 2:3], data = occ,
 #'                                      proj4string = WGS84)
 #'
-#' # reprojecting fro measuring distances
+#' # reprojecting for measuring distances
 #' LAEA <- LAEA_projection(occ_sp)
 #' occ_pr <- sp::spTransform(occ_sp, LAEA)
 #'
@@ -291,4 +325,91 @@ clusters <- function(occ_pr, cluster_method = "hierarchical", split_distance,
   occ_pr@data <- data.frame(occ_pr@data, clusters = cluster_vector)
 
   return(occ_pr)
+}
+
+
+#' Convex or concave hull polygons from spatial points
+#' @param occ_pr SpatialPoints* object containing geographic points to be used
+#' to create hull polygons. This spatial object must be projected to a system
+#' with th argument "+units=m".
+#' @param hull_type (character) type of hull polygons to be created. Available
+#' options are: "convex" and "concave". Default = "convex".
+#' @param concave_distance_lim (numeric) distance, in meters, to be passed to the
+#' length_threshold parameter of the \code{\link[concaveman]{concaveman}} funcion.
+#' Default = 5000. Ignored if \code{hull_type} is not "concave".
+#' @return A SpatialPolygon object with the hull polygon. If the number of points
+#' in occ_pr is 1 or 2 a SpatialPointsDataFrame object is returned.
+#' @export
+#' @importFrom sp CRS SpatialPointsDataFrame
+#' @importFrom sp SpatialPolygons Polygons Polygon
+#' @importFrom sf st_multipoint st_sf st_sfc as_Spatial st_zm
+#' @importFrom concaveman concaveman
+#' @examples
+#' # data
+#' data("occ_p", package = "rangemap")
+#'
+#' # preparing spatial points
+#' occ <- as.data.frame(unique(occ_p))
+#' WGS84 <- sp::CRS("+init=epsg:4326")
+#' occ_sp <- sp::SpatialPointsDataFrame(coords = occ[, 2:3], data = occ,
+#'                                      proj4string = WGS84)
+#'
+#' # reprojecting
+#' LAEA <- LAEA_projection(occ_sp)
+#' occ_pr <- sp::spTransform(occ_sp, LAEA)
+#'
+#' # convex hull polygon
+#' cvx_hull <- hull_polygon(occ_pr, hull_type = "convex")
+
+hull_polygon <- function(occ_pr, hull_type = "convex", concave_distance_lim = 5000) {
+  if (hull_type == "convex" | hull_type == "concave") {
+    condition <- !is.list(occ_pr)
+    if (condition) {
+      proje <- occ_pr@proj4string
+      occ_pr <- list(occ_pr)
+    } else {
+      proje <- occ_pr[[1]]@proj4string
+    }
+    if (is.na(proje)) {
+      stop("'occ_pr' must be projected.")
+    }
+    if (hull_type == "convex") {
+      message("Hull type: convex")
+
+      hulls <- lapply(1:length(occ_pr), function(i) {
+        coord <- as.data.frame(sp::coordinates(occ_pr[[i]]))
+        if (dim(coord)[1] > 2) {
+          covexhull <- chull(coord) # convex hull from points
+          coord_pol <- coord[c(covexhull, covexhull[1]),] # defining coordinates
+          hull <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(coord_pol)),
+                                                        ID = 1)), proj4string = proje)
+          #sp::proj4string(hull) <- AEQD # project
+        }else {
+          hull <- sp::SpatialPointsDataFrame(coords = coord, data = coord,
+                                             proj4string = proje)
+        }
+      })
+    } else {
+      message("Hull type: concave")
+      hulls <- lapply(1:length(occ_pr), function(i) {
+        coord <- as.data.frame(sp::coordinates(occ_pr[[i]]))
+        if (dim(coord)[1] > 2) {
+          sppoints <- sf::st_multipoint(as.matrix(coord))
+          sf_points <- sf::st_sf(sf::st_sfc(sppoints, crs = proje@projargs))
+          concavehull <- concaveman::concaveman(sf_points,
+                                                length_threshold = concave_distance_lim)
+          hull <- sf::as_Spatial(sf::st_zm(concavehull$polygons))
+        } else {
+          hull <- sp::SpatialPointsDataFrame(coords = coord, data = coord,
+                                             proj4string = proje)
+        }
+      })
+    }
+  } else {
+    stop(paste("'hull_type' is not valid, potential options are:\nconvex or concave"))
+  }
+  if (condition) {
+    hulls <- hulls[[1]]
+  }
+  return(hulls)
 }
--- R/rangemap_hull.R
+++ R/rangemap_hull.R
@@ -1,4 +1,4 @@
-#' Species distributional ranges based on distinct hull polygons
+#' Species distributional ranges based on convex or concave hull polygons
 #'
 #' @description rangemap_hull generates a distributional range for a given species
 #' by creating convex or concave hull polygons based on occurrence data.
@@ -8,46 +8,49 @@
 #'
 #' @param occurrences a data.frame containing geographic coordinates of species occurrences,
 #' columns must be: Species, Longitude, and Latitude. Geographic coordinates must be in decimal
-#' degrees.
+#' degrees  (WGS84).
 #' @param hull_type (character) type of hull polygons to be created. Available options are:
-#' "convex" or "concave" hulls. Default = "convex".
+#' "convex" and "concave". Default = "convex".
 #' @param concave_distance_lim (numeric) distance, in meters, to be passed to the length_threshold
 #' parameter of the \code{\link[concaveman]{concaveman}} funcion. Default = 5000. Ignored if
 #' \code{hull_type} is not "concave".
-#' @param buffer_distance (numeric) distance, in meters, to be used for creating the buffer
-#' areas around occurrences, default = 50000.
-#' @param split (logical) if TRUE, a distance (for hierarchical clustering) or a number (for
+#' @param buffer_distance (numeric) distance, in meters, to be used for creating a buffer
+#' around resulting hull polygons; default = 50000.
+#' @param split (logical) if \code{TRUE}, a distance (for hierarchical clustering) or a number (for
 #' K-means clustering) is used to separate distinct chunks of occurrences. Recommended when
-#' the species of interest has a disjunct distribution. Default = FALSE.
+#' the species of interest has a disjunct distribution. Default = \code{FALSE}.
 #' @param cluster_method (character) name of the method to be used for clustering the occurrences.
 #' Options are "hierarchical" and "k-means"; default = "hierarchical". Note that this parameter is
-#' ignored when \code{split} = FALSE. See details for more information on the two available methods.
+#' ignored when \code{split} = \code{FALSE}. See details for more information on the two available methods.
 #' @param split_distance (numeric) distance in meters that will limit connectivity among hull
 #' polygons created with chunks of points separated by long distances. This parameter is used
-#' when \code{cluster_method} = "hierarchical" and \code{split} = TRUE.
-#' @param n_k_means (numeric) if \code{split} = TRUE, number of clusters in which the species
+#' when \code{cluster_method} = "hierarchical" and \code{split} = \code{TRUE}.
+#' @param n_k_means (numeric) if \code{split} = \code{TRUE}, number of clusters in which the species
 #' occurrences will be grouped when using the "k-means" \code{cluster_method}.
 #' @param polygons (optional) a SpatialPolygon object to clip buffer areas and adjust the species
-#' range and other polygons to these limits. Projection must be Geographic (EPSG:4326).
+#' range and other polygons to these limits. Projection must be WGS84 (EPSG:4326).
 #' If NULL (the default), a simplified world map will be used.
 #' @param final_projection (character) string of projection arguments for resulting Spatial objects.
 #' Arguments must be as in the PROJ.4 documentation. See \code{\link[sp]{CRS-class}} for details.
 #' Default = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0" = WGS84.
-#' @param save_shp (logical) if TRUE, shapefiles of the species range, occurrences, extent of
-#' occurrence and area of occupancy will be written in the working directory. Default = FALSE.
-#' @param name (character) valid if \code{save_shp} = TRUE. The name of the shapefile to be
+#' @param save_shp (logical) if \code{TRUE}, shapefiles of the species range, occurrences, extent of
+#' occurrence and area of occupancy will be written in the working directory. Default = \code{FALSE}.
+#' @param name (character) valid if \code{save_shp} = \code{TRUE}. The name of the shapefile to be
 #' exported. A suffix will be added to \code{name} depending on the object as follows: species
 #' extent of occurrence = "_extent_occ", area of occupancy = "_area_occ", and occurrences =
 #' "_unique_records".
+#' @param overwrite (logical) wether or not to overwrite previous results with
+#' the same name. Default = \code{FALSE}.
 #'
 #' @return
-#' A named list containing: (1) a data.frame with information about the species range,
+#' A sp_range object (S4) containing: (1) a data.frame with information about the species range,
 #' and SpatialPolygon objects of (2) unique occurrences, (3) species range, (4) extent of
 #' occurrence, and (5) area of occurpancy.
 #'
 #' @details
-#' All resultant Spatial objects in the list of results will be projected to the \code{final_projection}.
-#' Areas are calculated in square kilometers using the Azimuthal equal area projection.
+#' All resultant Spatial objects in the results will be projected to the \code{final_projection}.
+#' Areas are calculated in square kilometers using the Lambert Azimuthal Equal
+#' Area projection, centered in the centroid of occurence points given as imputs.
 #'
 #' The \code{cluster_method} must be chosen based on the spatial configuration of the
 #' species occurrences. Both methods make distinct assumptions and one of them may perform better
@@ -63,66 +66,38 @@
 #' problematic when trying to replicate your methods. With hierarchical clustering, most likely
 #' the same clusters can be obtained if the process is repeated.
 #'
-#' For more information on these clustering methods see Aggarwal and Reddy (2014)
-#' \url{https://goo.gl/RQ2ebd}.
+#' For more information on these clustering methods see Aggarwal and Reddy (2014),
+#' \href{https://goo.gl/RQ2ebd}{here}.
 #'
 #' @usage
-#' rangemap_hull(occurrences, hull_type = "convex", buffer_distance = 50000,
-#'     concave_distance_lim = 5000, split = FALSE, cluster_method = "hierarchical",
-#'     split_distance, n_k_means, polygons, final_projection, save_shp = FALSE,
-#'     name = "range_hull", overwrite = FALSE)
+#' rangemap_hull(occurrences, hull_type = "convex", concave_distance_lim = 5000,
+#'               buffer_distance = 50000, split = FALSE, cluster_method = "hierarchical",
+#'               split_distance, n_k_means, polygons = NULL, final_projection = NULL,
+#'               save_shp = FALSE, name, overwrite = FALSE)
 #'
 #' @export
 #'
 #' @importFrom sp CRS SpatialPointsDataFrame SpatialPolygonsDataFrame
-#' @importFrom sp SpatialPolygons Polygons Polygon proj4string over spTransform
-#' @importFrom sp rbind.SpatialPolygons
-#' @importFrom raster disaggregate area extent rasterize
-#' @importFrom rgeos gCentroid gUnaryUnion gIntersection gBuffer
-#' @importFrom maps map
-#' @importFrom maptools map2SpatialPolygons
+#' @importFrom sp spTransform rbind.SpatialPolygons
+#' @importFrom raster disaggregate area
+#' @importFrom rgeos gUnaryUnion gIntersection gBuffer
 #' @importFrom rgdal writeOGR
-#' @importFrom sf st_multipoint st_sf st_sfc as_Spatial st_zm
-#' @importFrom concaveman concaveman
 #'
 #' @examples
-#' suppressWarnings({if(!require(spocc)){
-#'   install.packages("spocc")
-#'   library(spocc)
-#' }})
+#' # getting the data
+#' data("occ_d", package = "rangemap")
 #'
-#' # getting the data from GBIF
-#' occs <- occ(query = "Dasypus kappleri", from = "gbif",
-#'             limit = 1000)$gbif$data[[1]]
-#'
-#' # keeping only georeferenced records
-#' occ_g <- occs[!is.na(occs$latitude) & !is.na(occs$longitude),
-#'               c("name", "longitude", "latitude")]
-#'
+#' # other info for running
 #' dist <- 100000
 #' hull <- "convex" # try also "concave"
-#' split <- TRUE
-#' c_method <- "hierarchical"
-#' split_d <- 1500000
-#' save <- TRUE
-#' name <- "test"
 #'
-#' hull_range <- rangemap_hull(occurrences = occ_g, hull_type = hull, buffer_distance = dist,
-#'                             split = split, cluster_method = c_method, split_distance = split_d,
-#'                             save_shp = save, name = name, overwrite = TRUE)
+#' hull_range <- rangemap_hull(occurrences = occ_d, hull_type = hull,
+#'                             buffer_distance = dist)
 #'
-#' # see the species range in a figure
-#' extent <- TRUE
-#' occ <- TRUE
-#' legend <- TRUE
-#' north <- TRUE
-#'
-#' # creating the species range figure
-#' rangemap_fig(hull_range, add_extent = extent, add_occurrences = occ,
-#'              legend = legend, northarrow = north)
+#' summary(hull_range)
 
-rangemap_hull <- function(occurrences, hull_type = "convex", buffer_distance = 50000,
-                          concave_distance_lim = 5000, split = FALSE,
+rangemap_hull <- function(occurrences, hull_type = "convex", concave_distance_lim = 5000,
+                          buffer_distance = 50000, split = FALSE,
                           cluster_method = "hierarchical", split_distance, n_k_means,
                           polygons = NULL, final_projection = NULL, save_shp = FALSE,
                           name, overwrite = FALSE) {
@@ -142,30 +117,39 @@ rangemap_hull <- function(occurrences, hull_type = "convex", buffer_distance = 5
       stop("Argument 'n_k_means' must be defined when k-means cluster method is used.")
     }
   }
+  if (save_shp == TRUE) {
+    if (missing(name)) {
+      stop("Argument 'name' must be defined if 'save_shp' = TRUE.")
+    }
+    if (file.exists(paste0(name, ".shp")) & overwrite == FALSE) {
+      stop("Files already exist, use 'overwrite' = TRUE.")
+    }
+  }
 
   # erase duplicate records
   occ <- as.data.frame(unique(occurrences))[, 1:3]
   colnames(occ) <- c("Species", "Longitude", "Latitude")
 
   # make a spatial object from coordinates
-  WGS84 <- sp::CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+  WGS84 <- sp::CRS("+init=epsg:4326")
   occ_sp <- sp::SpatialPointsDataFrame(coords = occ[, 2:3], data = occ,
                                        proj4string = WGS84)
 
   # world map or user map fro creating species range
   if (is.null(polygons)) {
-    polygons <- simple_wmap()
+    polygons <- simple_wmap(which = "simple")
   }
 
   # keeping only records in land
   occ_sp <- occ_sp[polygons, ]
 
   # project the points
-  ECK4 <- sp::CRS("+proj=eck4 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
-  occ_pr <- sp::spTransform(occ_sp, ECK4)
+  AEQD <- AED_projection(spatial_onject = occ_sp)
+  occ_pr <- sp::spTransform(occ_sp, AEQD)
 
   # project polygons
-  polygons <- sp::spTransform(polygons, ECK4)
+  LAEA <- LAEA_projection(spatial_onject = occ_sp)
+  polygons <- sp::spTransform(polygons, LAEA)
 
   # clustering
   if (split == TRUE) {
@@ -175,82 +159,35 @@ rangemap_hull <- function(occurrences, hull_type = "convex", buffer_distance = 5
   }
 
   ## splitting points
-  if (sum(unique(occ_pr@data$clusters)) > 1) {
-    occ_prs <- split(occ_pr, occ_pr@data$clusters)
+  occ_pr <- sp::spTransform(occ_pr, LAEA)
+  clust <- sort(unique(occ_pr@data$clusters))
+  if (length(clust) > 1) {
+    occ_prs <- lapply(clust, function(x) {occ_pr[occ_pr@data$clusters == x, ]})
   }else {
     occ_prs <- list(occ_pr)
   }
 
   # create hulls depending in the user-defined argument (convex, concave, and alpha)
   # and per each group of points if they were clustered
-  if (hull_type == "convex" | hull_type == "concave") {
-    if (hull_type == "convex") {
-      message("Hull type: convex")
-
-      hulls <- list()
-
-      for (i in 1:length(occ_prs)) {
-        if (length(occ_prs) > 1) {
-          coord <- as.data.frame(occ_prs@coords[[i]]) # spatial point dataframe to data frame keeping only coordinates
-        }else {
-          coord <- as.data.frame(sp::coordinates(occ_prs[[i]])) # spatial point dataframe to data frame keeping only coordinates
-        }
-
-        if (dim(coord)[1] > 2) {
-          covexhull <- chull(coord) # convex hull from points
-          coord_pol <- coord[c(covexhull, covexhull[1]),] # defining coordinates
-          hulls[[i]] <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(coord_pol)), ID = 1))) # into SpatialPolygons
-          sp::proj4string(hulls[[i]]) <- AEQD # project
-        }else {
-          hulls[[i]] <- sp::SpatialPointsDataFrame(coords = coord, data = coord,
-                                                   proj4string = AEQD)
-        }
-      }
-    }
-    if (hull_type == "concave") {
-      message("Hull type: concave")
-
-      hulls <- list()
-
-      for (i in 1:length(occ_prs)) {
-        if (length(occ_prs) > 1) {
-          coord <- as.data.frame(occ_prs@coords[[i]]) # spatial point dataframe to data frame keeping only coordinates
-        }else {
-          coord <- as.data.frame(sp::coordinates(occ_prs[[i]])) # spatial point dataframe to data frame keeping only coordinates
-        }
-
-        if (dim(coord)[1] > 2) {
-          sppoints <- sf::st_multipoint(as.matrix(coord)) #sf_multipoints from multypoints
-          sf_points <- sf::st_sf(sf::st_sfc(sppoints, crs = AEQD@projargs))
-          concavehull <- concaveman::concaveman(sf_points, length_threshold = concave_distance_lim) # concave hull from points
-          hulls[[i]] <- sf::as_Spatial(sf::st_zm(concavehull$polygons)) # into SpatialPolygons
-        }else {
-          hulls[[i]] <- sp::SpatialPointsDataFrame(coords = coord, data = coord,
-                                                   proj4string = AEQD)
-        }
-      }
-    }
-  }else {
-    stop(paste("'hull_type' is not valid, potential options are:\nconvex or concave"))
-  }
+  hulls <- hull_polygon(occ_prs, hull_type, concave_distance_lim)
 
   # create a buffer based on a user-defined distance
-  hulls_buffer <- list()
+  hulls_buffer <- lapply(hulls, rgeos::gBuffer, width = buffer_distance)
 
-  for (i in 1:length(hulls)) {
-    hulls_buffer[[i]] <- rgeos::gBuffer(hulls[[i]], width = buffer_distance)
-  }
+  #for (i in 1:length(hulls)) {
+  #  hulls_buffer[[i]] <- rgeos::gBuffer(hulls[[i]], width = buffer_distance)
+  #}
 
   # union buffered polygons
-  hulls_buff_un <- do.call(sp::rbind.SpatialPolygons, c(hulls_buffer, list(makeUniqueIDs = TRUE)))
+  hulls_buff_un <- do.call(sp::rbind.SpatialPolygons,
+                           c(hulls_buffer, list(makeUniqueIDs = TRUE)))
 
   # Clipping with the world
   polygons <- suppressWarnings(rgeos::gBuffer(polygons, byid = TRUE, width = 0)) # to avoid topology problems
   polygons <- rgeos::gUnaryUnion(polygons)
 
-  hulls_buff_un <- suppressWarnings(rgeos::gIntersection(hulls_buff_un, polygons,
-                                                         byid = TRUE, drop_lower_td = TRUE)) # area of interest
-
+  hulls_buff_un <- rgeos::gIntersection(hulls_buff_un, polygons, byid = TRUE,
+                                        drop_lower_td = TRUE) # area of interest
   hulls_buff_un <- raster::disaggregate(hulls_buff_un)
 
   # calculate areas in km2
@@ -298,13 +235,14 @@ rangemap_hull <- function(occurrences, hull_type = "convex", buffer_distance = 5
                     driver = "ESRI Shapefile", overwrite_layer = overwrite)
   }
 
-  # return results (list or a different object?)
+  # return results
   sp_dat <- data.frame(Species = species, Unique_records = dim(occ_pr)[1],
                        Range_area = areackm2, Extent_of_occurrence = eocckm2,
                        Area_of_occupancy = aocckm2)
 
-  results <- list(Summary = sp_dat, Species_unique_records = occ_pr,
-                  Species_range = clip_area, Extent_of_occurrence = extent_occurrence,
-                  Area_of_occupancy = area_occupancy)
+  results <- sp_range_iucn(Summary = sp_dat, Species_unique_records = occ_pr,
+                           Species_range = clip_area,
+                           Extent_of_occurrence = extent_occurrence,
+                           Area_of_occupancy = area_occupancy)
   return(results)
 }
