ad8d8e82b057466132aac3a4db041029071287bf
marlonecobos <manubio13@gmail.com>
2020-05-27 22:36:32 -0500

    V 0.1.6 data documentation occurrences 2 spp

--- R/occ_doc.R
+++ R/occ_doc.R
@@ -14,3 +14,21 @@
 #' data(occ_p)
 #' head(occ_p)
 "occ_p"
+
+#' Occurrence records for the species Dasypus kappleri
+#'
+#' @description A dataset containing geographic corrdinates of a South American
+#' armadillo.
+#'
+#' @format A data frame with 306 rows and 3 columns.
+#' \describe{
+#'   \item{name}{character, species scientific name.}
+#'   \item{longitude}{numeric, longitude values.}
+#'   \item{latitude}{numeric, latitude values.}
+#' }
+#' @source \url{https://www.gbif.org/}
+#'
+#' @examples
+#' data(occ_d)
+#' head(occ_d)
+"occ_d"
