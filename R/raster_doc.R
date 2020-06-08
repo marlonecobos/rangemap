ae83d13da023eb32470b204dbd30193999a3bdaa
marlonecobos <manubio13@gmail.com>
2020-06-07 19:12:46 -0500

    V 0.1.11 raster data documented

---
+++ R/raster_doc.R
@@ -0,0 +1,45 @@
+#' An ecological niche model created with Maxent
+#'
+#' A RasterLayer containing an ecological niche model for the a tick
+#' (*Amblyomma americanum*).
+#'
+#' @format A RasterLayer with 150 rows, 249 columns, and 37350 cells:
+#' \describe{
+#'   \item{Suitability}{suitability, in probability values.}
+#' }
+#'
+#' @source \url{https://kuscholarworks.ku.edu/handle/1808/26376}
+#'
+#' @name sp_model
+#'
+#' @examples
+#' model <- raster::raster(list.files(system.file("extdata", package = "rangemap"),
+#'                                    pattern = "sp_model.tif", full.names = TRUE))
+#'
+#' raster::plot(model)
+
+
+
+#' A set of environmental variables for examples
+#'
+#' A RasterStack containing four bioclimatic variables downloaded from the
+#' WorldClim database 1.4.
+#'
+#' @format A RasterStack with 180 rows, 218 columns, 39240 cells, and 4 layers:
+#' \describe{
+#'   \item{variables.1}{bio5.}
+#'   \item{variables.2}{bio6.}
+#'   \item{variables.3}{bio13.}
+#'   \item{variables.4}{bio14.}
+#' }
+#'
+#' @source \url{https://www.worldclim.org/data/v1.4/worldclim14.html}
+#'
+#' @name variables
+#'
+#' @examples
+#' vars <- raster::stack(system.file("extdata", "variables.tif",
+#'                                   package = "rangemap"))
+#' names(vars) <- c("bio5", "bio6", "bio13", "bio14")
+#'
+#' raster::plot(vars)
