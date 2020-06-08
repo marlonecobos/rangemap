00f8e54ac2154bb7c4d5433fb41fc81cc378be68
marlonecobos <manubio13@gmail.com>
2020-06-07 23:48:22 -0500

    V 0.1.12 ranges_espace complete update

--- R/ranges_espace.R
+++ R/ranges_espace.R
@@ -1,299 +1,188 @@
 #' Comparison of species ranges in environmental space
 #'
-#' @description ranges_espace generates a three dimensional comparison of a species'
-#' ranges created using distinct algortihms, to visualize implications of selecting
-#' one of them if environmental conditions are considered.
+#' @description ranges_espace generates a three dimensional comparison of
+#' distributional ranges for a species created using distinct functions of
+#' \code{\link{rangemap}}, to visualize them in environmental conditions.
 #'
-#' @param ranges object or list of objects produced with any of the following functions:
-#' \code{\link{rangemap_buff}}, \code{\link{rangemap_bound}}, \code{\link{rangemap_hull}},
-#' \code{\link{rangemap_enm}}, and \code{\link{rangemap_tsa}}. For visualization purposes,
-#' using up to three ranges is recommended.
-#' @param add_occurrences (logical) if TRUE, species occurrences contained in one of
-#' the elements of the list \code{ranges} will be ploted in the figure. Default = TRUE.
-#' If the none of the ranges contains occurrences (e.g. a list of one object created
-#' with the \code{\link{rangemap_bound}} function in which occurrences were not used),
-#' this parameter will be ignored.
-#' @param variables a RasterStack object of environmental variables that will be used
-#' for creating the principal components to represent the environmental space. Projection
-#' is assumed to be Geographic (longitude and latitude).
-#' @param max_background (numeric) maximum number of data from variables to be used for
-#' representation of the environmental space. Default = 25000. Increasing this number
-#' results in more detailed views of the available environment but preforming analyses
-#' will take longer.
-#' @param ranges_representation (character) form in which the environmental space
-#' withing the ranges will be represented. Options are "clouds" and "ellipsoids".
-#' Default = "clouds".
-#' @param background_color color of the background to be ploted. Default = "darkolivegreen".
-#' Since transparency is used for representing most of components of the plot, colors may
-#' look different.
-#' @param range_colors vector of colors of the ranges to be represented. If not defined,
-#' default = NULL and default colors will be used. Since transparency is used for
-#' representing most of components of the plot, colors may look different.
-#' @param alpha (numeric) degree of opacity with which the species ranges will be
-#' represented in the plot. Default = 0.03.
-#' @param eye_camera (numeric) vector of values to be passed to eye in the list of
-#' arguments of the parameter camera in layour of \code{\link[plotly]{plotly}}. This
-#' values control how the figur is seen in the plot.
+#' @param ... one or more objects of class \code{\link{sp_range}} produced with
+#' any of the following functions: \code{\link{rangemap_buffer}},
+#' \code{\link{rangemap_boundaries}}, \code{\link{rangemap_hull}},
+#' \code{\link{rangemap_enm}}, and/or \code{\link{rangemap_tsa}}. Using up to
+#' five ranges is allowed for more precise comparisons.
+#' @param add_occurrences (logical) if \code{TRUE}, species occurrences contained
+#' in one of the \code{sp_range} objects will be ploted. Default = \code{TRUE}.
+#' If  none of the objects contains occurrences this argument will be ignored.
+#' @param variables a RasterStack object of at least 3 environmental variables
+#' that will be used to perform a principal component analisis, and use the 3
+#' first principal components to represent the environmental space. Projection
+#' is assumed to be WGS84 (EPSG:4326).
+#' @param max_background (numeric) maximum number of data from variables to be
+#' used for representing the environmental space. Default = 10000.
+#' @param occurrence_color color for ocurrence records in environmental space.
+#' @param range_colors vector of colors for the ranges to be represented. If
+#' \code{NULL}, the default, a set of colors will be used. Since transparency is
+#' used for representing ranges in the plot, colors may look different.
+#' @param alpha (numeric) degree of opacity for plotting species ranges.
+#' Default = 0.6.
+#' @param legend (logical) if \code{TRUE}, a simple legend will be added.
+#' Default = \code{TRUE}.
 #'
-#' @return A figure showing, in the environmental space, the species ranges generated with
-#' any of the functions: \code{\link{rangemap_buff}}, \code{\link{rangemap_bound}},
-#' \code{\link{rangemap_hull}}, \code{\link{rangemap_enm}}, and \code{\link{rangemap_tsa}}.
+#' @return
+#' A figure showing distributional ranges of a species represented in environmental
+#' space (3 principal components).
 #'
 #' @usage
-#' ranges_espace(ranges, add_occurrences = TRUE, variables, max_background = 25000,
-#'     ranges_representation = "clouds", background_color = "darkolivegreen",
-#'     range_colors = NULL, alpha = 0.03, eye_camera = c(x = 1.95, y = 1.25, z = 1.35))
+#' ranges_espace(..., add_occurrences = TRUE, variables,
+#'               max_background = 10000, occurrence_color = "blue",
+#'               range_colors = NULL, alpha = 0.6, legend = TRUE)
 #'
 #' @export
 #'
-#' @importFrom sp CRS SpatialPointsDataFrame spTransform
+#' @importFrom sp CRS SpatialPointsDataFrame
 #' @importFrom raster rasterToPoints extract
-#' @importFrom plotly plot_ly add_trace layout
-#' @importFrom rgl ellipse3d
+#' @importFrom rgl ellipse3d plot3d legend3d aspect3d
+#' @importFrom stats prcomp
 #'
 #' @examples
-#' suppressWarnings({if(!require(spocc)){
-#'   install.packages("spocc")
-#'   library(spocc)
-#' }})
+#' # example data
+#' data("buffer_range", package = "rangemap")
+#' data("cxhull_range", package = "rangemap")
 #'
-#' # getting the data from GBIF
-#' occs <- occ(query = "Dasypus kappleri", from = "gbif",
-#'             limit = 1000)$gbif$data[[1]]
-#'
-#' # keeping only georeferenced records
-#' occ_g <- occs[!is.na(occs$latitude) & !is.na(occs$longitude),
-#'               c("name", "longitude", "latitude")]
-#'
-#'
-#' # range based on buffers
-#' dist <- 500000
-#'
-#' buff <- rangemap_buff(occurrences = occ_g, buffer_distance = dist)
-#'
-#'
-#' # range based on concave hulls
-#' dist1 <- 250000
-#' hull1 <- "concave"
-#'
-#' concave <- rangemap_hull(occurrences = occ_g, hull_type = hull1, buffer_distance = dist1)
-#'
-#'
-#' # ranges comparison in environmental space
-#' ## list of ranges
-#' ranges <- list(buff, concave)
-#' names(ranges) <- c("buff", "concave")
-#'
-#' ## other data for environmental comparisson
-#' suppressWarnings({if(!require(raster)){
-#'   install.packages("raster")
-#'   library(raster)
-#' }
-#' if(!require(maps)){
-#'   install.packages("maps")
-#'   library(maps)
-#' }
-#' if(!require(maptools)){
-#'   install.packages("maptools")
-#'   library(maptools)
-#' }})
-#'
-#' vars <- getData("worldclim", var = "bio", res = 5)
-#'
-#' ## mask variables to region of interest
-#' WGS84 <- CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
-#' w_map <- map(database = "world", regions = c("Ecuador", "Peru", "Bolivia", "Colombia", "Venezuela",
-#'                                              "Suriname", "Guyana", "French Guyana", "Brazil"),
-#'              fill = TRUE, plot = FALSE) # map of the world
-#' w_po <- sapply(strsplit(w_map$names, ":"), function(x) x[1]) # preparing data to create polygon
-#' reg <- map2SpatialPolygons(w_map, IDs = w_po, proj4string = WGS84) # map to polygon
-#'
-#' e <- extent(reg)
-#' mask <- as(e, 'SpatialPolygons')
-#'
-#' variables <- crop(vars, mask)
+#' vars <- raster::stack(system.file("extdata", "variables.tif",
+#'                                   package = "rangemap"))
+#' names(vars) <- c("bio5", "bio6", "bio13", "bio14")
 #'
 #' ## comparison
-#' occur <- TRUE
-#' env_comp <- ranges_espace(ranges = ranges, add_occurrences = occur, variables = variables)
-#'
-#'
-#' # now play around, zoom in and rotate the figure
-#'
-#' # saving this figure may be challenging, try using
-#' # the following lines of code and check your download directory
-#'
-#' op <- options() # save default options
-#' options(viewer = NULL) # Set viewer to web browser
-#' name <- "ranges_space" # name for figure
-#'
-#' # using web browser to save image
-#' env_comp %>% htmlwidgets::onRender(
-#'   paste("function(el, x)
-#'           {var gd = document.getElementById(el.id);
-#'           Plotly.downloadImage(gd, {format: 'svg', width: ", 1000, ", height: ",
-#'         800, ", filename: ", paste("\'", name, "\'", sep = ""), "});
-#'           }", sep = "")
-#' )
-#'
-#' Sys.sleep(2) # wait for the execution
-#' options(viewer = op$viewer) # restore viewer to old setting (e.g. RStudio)
+#' ranges_espace(buffer_range, cxhull_range, variables = vars,
+#'               add_occurrences = TRUE)
 
-ranges_espace <- function(ranges, add_occurrences = TRUE, variables, max_background = 25000,
-                          ranges_representation = "clouds", background_color = "darkolivegreen",
-                          range_colors = NULL, alpha = 0.03, eye_camera = c(x = 1.95, y = 1.25, z = 1.35)) {
+ranges_espace <- function(..., add_occurrences = TRUE, variables,
+                          max_background = 10000, occurrence_color = "blue",
+                          range_colors = NULL, alpha = 0.6, legend = TRUE) {
 
   # testing potential issues
-  if (missing(ranges)) {
-    stop("ranges must exist. See the function's help for more details.")
+  if (missing(...)) {
+    stop("Argument '...' is necessary to perform the analysis")
+  } else {
+    ranges <- list(...)
+    if (length(ranges) < 1) {
+      stop("At least one sp_range* object is needed to produce plots.")
+    }
+    if (length(ranges) > 5) {
+      stop("Only 5 sp_range* objects can be plotted at the time.")
+    }
+    cls <- sapply(ranges, function(x) {class(x)[1]})
+    if (any(!cls %in% c("sp_range", "sp_range_iucn", "sp_range_iucnextra"))) {
+      stop("All objects to be plotted must be of class sp_range*.")
+    }
+  }
+  if (missing(variables)) {
+    stop("Argument 'variables' must be defined. See function's help for details.")
   }
 
   # preparing data
   ## plain projection
-  WGS84 <- sp::CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
-
-  ## unlist nested lists
-  r <- lapply(ranges, unlist)
+  WGS84 <- ranges[[1]]@species_range@proj4string
 
   ## extracting data
+  sp_ranges <- lapply(ranges, function(x) {x@species_range})
+
+  rnames <- sapply(ranges, function(x) {x@name})
+
   if (add_occurrences == TRUE) {
-    lranges <- sapply(ranges, length)
+    if (any(cls %in% c("sp_range_iucn", "sp_range_iucnextra"))){
+      wh <- which(cls %in% c("sp_range_iucn", "sp_range_iucnextra"))[1]
 
-    if (any(lranges > 2)){
-      n <- which(lranges > 2)[1]
       ## occurrences
-      if (isnested(r)) {
-        sp_rec <- ranges[[n]]$Species_unique_records
-      }else {
-        sp_rec <- ranges$Species_unique_records
-      }
-      occ_sp <- sp::spTransform(sp_rec, WGS84)
-      occ <- as.data.frame(occ_sp@data)[, 1:3]
-      colnames(occ) <- c("Species", "Longitude", "Latitude")
+      sp_records <- ranges[[wh]]@species_unique_records
 
-      occ1 <- occ[, 2:3]
-      colnames(occ1) <- c("x", "y")
-
-      ## varaible data in occurrences
-      pdata <- na.omit(cbind(occ1, raster::extract(variables, occ1)))
-
-    }else {
-      warning("None of the objects in \"range\" contain occurrences, \"add_occurrences = TRUE\" ignored.")
+      ## variables in occurrences
+      pdata <- na.omit(cbind(sp_records@data[, 2:3],
+                             raster::extract(variables, sp_records)))
+    } else {
       add_occurrences <- FALSE
+      sp_records <- NULL
+      message("None of the objects contain occurrences.")
     }
+  } else {
+    sp_records <- NULL
   }
 
+  ## projecting
+  variables <- raster::projectRaster(variables, crs = WGS84)
+
   ## raster to varaibles data
-  idata <- raster::rasterToPoints(variables)
+  vdata <- raster::rasterToPoints(variables)
+
+  if (nrow(vdata) > max_background) {
+    vdata <- vdata[sample(nrow(vdata), max_background), ]
+  }
+  colnames(vdata)[1:2] <- colnames(pdata)[1:2]
+
+  nr <- nrow(vdata)
+  nc <- ncol(vdata)
 
   if (add_occurrences == TRUE) {
-      ## combining these data
-      vdata <- rbind(idata, pdata)
+    ## combining these data
+    vdata <- rbind(vdata, pdata)
   }else {
     vdata <- idata
   }
 
   # pca
   ## pca with vdata
-  pcav <- prcomp(na.omit(vdata[, 3:dim(vdata)[2]]), center = TRUE,
-                 scale = TRUE)
+  vdata <- data.frame(vdata[, 1:2], prcomp(vdata[, 3:nc], center = TRUE,
+                                           scale = TRUE)$x[, 1:3])
 
-  ## getting the data of components in points
-  pca_scores = pcav$x
-  pc3 <- data.frame(vdata[, 1:2], pca_scores[, 1:3])
+  nc <- 5
 
   if (add_occurrences == TRUE) {
-    pc_occ <- pc3[(length(pc3[, 1]) - length(pdata[, 1]) + 1):length(pc3[, 1]), ]
-    pc_points <- sp::SpatialPointsDataFrame(coords = pc_occ[, 1:2], data = pc_occ[, 3:dim(pc3)[2]],
-                                            proj4string = WGS84)
+    pc_occ <- vdata[(nr - nrow(pdata) + 1):nrow(vdata), 3:nc]
   }
 
-  if (dim(pc3)[1] > max_background) {
-    pc3 <- pc3[sample(row.names(pc3), max_background), ]
-  }
-
-  pc_var <- sp::SpatialPointsDataFrame(coords = pc3[, 1:2], data = pc3[, 3:dim(pc3)[2]],
-                                       proj4string = WGS84)
+  vdata <- sp::SpatialPointsDataFrame(coords = vdata[, 1:2], data = vdata[, 1:nc],
+                                      proj4string = WGS84)
 
   # getting the species ranges from objects in ranges, and
   # getting environmental (PCs) data in ranges
-  if (isnested(r)) {
-    cat("\nGetting environmental conditions inside ranges, please wait...\n")
-    env_ranges <- list()
-    for (i in 1:length(ranges)) {
-      sp_ranges <- sp::spTransform(ranges[[i]]$Species_range, WGS84)
-      env_ranges[[i]] <- pc_var[sp_ranges, ]
-      cat("Progress: ", i, "of", length(ranges) ,"\n")
-    }
-  }else {
-    sp_ranges <- sp::spTransform(ranges$Species_range, WGS84)
-    cat("\nGetting environmental conditions inside range, please wait...\n")
-    env_ranges <- pc_var[sp_ranges, ]
-  }
-
-  rnames <- names(ranges)
+  message("Getting environmental conditions inside ranges, please wait...")
+  env_ranges <- lapply(sp_ranges, function(x) {vdata[x, ]@data})
 
   # range colors
   if (is.null(range_colors)) {
-    colors <- c("darkorange", "mediumblue", "pink", "turquoise1", "black", "purple", "green")
+    colors <- c("black", "green", "red", "blue", "purple")
   }else {
     colors <- range_colors
   }
 
   # plot
-  cat("\nCreating an interactive visualization...\n")
-  if (ranges_representation == "clouds") {
-    opa <- 0.06
-    opa1 <- alpha
-  }
-  if (ranges_representation == "ellipsoids") {
-    opa <- 0.06
-    opa1 <- alpha
-  }
+  message("\nCreating interactive visualization...\n")
 
-  p <- plotly::plot_ly()
-  p <- plotly::add_trace(p, x = pc_var$PC1, y = pc_var$PC2, z = pc_var$PC3, mode = "markers", type = "scatter3d",
-                         marker = list(size = 3, color = background_color, opacity = opa, symbol = 104),
-                         name = "Available space") %>%
-    plotly::layout(scene = list(xaxis = list(title = "PC 1", backgroundcolor="white", showbackground=TRUE,
-                                     titlefont = list(color = "black", family = "Arial", size = 15)),
-                        yaxis = list(title = "PC 2", backgroundcolor="white", showbackground=TRUE,
-                                     titlefont = list(color = "black", family = "Arial", size = 15)),
-                        zaxis = list(title = "PC 3", backgroundcolor="white", showbackground=TRUE,
-                                     titlefont = list(color = "black", family = "Arial", size = 15)),
-                        camera = list(eye = list(x = eye_camera[1], y = eye_camera[2], z = eye_camera[3]))),
-                   legend = list(orientation = "h"))
-
-  if (ranges_representation == "clouds") {
-    for(i in 1:length(env_ranges)) {
-      env <- env_ranges[[i]]@data
-      p <- plotly::add_trace(p, x = env$PC1, y = env$PC2, z = env$PC3,
-                             type = "scatter3d", mode = "markers",
-                             marker = list(size = 4, color = colors[i],
-                                           opacity = opa1, symbol = 104), name = paste("Range", rnames[i]))
-    }
-  }
-  if (ranges_representation == "ellipsoids") {
-    for(i in 1:length(env_ranges)) {
-      ell <- rgl::ellipse3d(cov(env_ranges[[i]]@data))
-      p <- plotly::add_trace(p, x = ell$vb[1, ], y = ell$vb[2, ], z = ell$vb[3, ],
-                             type = "scatter3d", size = 1,
-                             mode = "markers",
-                             marker = list(color = colors[i],
-                                           opacity = opa1), name = paste("Range", rnames[i]))
-    }
-  }
+  ## interactive plot
   if (add_occurrences == TRUE) {
-    points <- pc_points@data
-    p <- plotly::add_trace(p, x = points$PC1, y = points$PC2, z = points$PC3, mode = "markers", type = "scatter3d",
-                           marker = list(size = 5, color = "black", symbol = 104), name = "Occurrences")
+    rgl::plot3d(pc_occ[, 1:3], col = occurrence_color, size = 6, xlab = "PC 1",
+                ylab = "PC 2", zlab = "PC 3")
+    ad <- TRUE
+  }else {
+    ad <- FALSE
   }
 
-  # present the figure
-  print(p)
+  ## ellipsoids
+  ellipsoides <- lapply(1:length(env_ranges), function(x) {
+    centroid <- apply(env_ranges[[x]][, 3:nc], 2, mean)
+    cov_mat <- cov(env_ranges[[x]][, 3:nc])
+    ell <- rgl::ellipse3d(cov_mat, centre = centroid, level = 0.99)
 
-  cat("\nFor further work with the figure use the object created with the function.\n")
+    adt <- ifelse(x == 1, ad, TRUE)
+    rgl::wire3d(ell, col = colors[x], alpha = alpha, xlab = "PC 1",
+                ylab = "PC 2", zlab = "PC 3", add = adt)
+  })
 
-  # return results
-  return(p)
+  ## legend
+  if (legend == TRUE) {
+    rgl::legend3d("topright", legend = rnames, lty = 1, inset = 0.02,
+                  col = colors[1:length(ranges)],
+                  bty = "n")
+  }
+  rgl::aspect3d(1, 1, 1)
 }
